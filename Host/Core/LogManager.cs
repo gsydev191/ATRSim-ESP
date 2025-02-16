3f16a51f6b7b55bfb2c73b2882197ef7f432538c
Dan <gsygaming@gmail.com>
2025-02-16 13:12:59 +0000

    Initial commit

---
+++ Host/Core/LogManager.cs
@@ -0,0 +1,46 @@
+﻿using Serilog;
+
+namespace Host;
+
+public static class LogManager
+{
+    private static readonly ILogger Logger;
+
+    static LogManager()
+    {
+        Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+    }
+
+    public static void Info(string message)
+    {
+        Logger.Information(message);
+    }
+
+    public static void Error(string message)
+    {
+        Logger.Error(message);
+    }
+
+    public static void Error(string message, Exception exception)
+    {
+        Logger.Error(exception, message);
+    }
+
+    public static void Warning(string message)
+    {
+        Logger.Warning(message);
+    }
+
+    public static void Fatal(string message)
+    {
+        Logger.Fatal(message);
+    }
+
+    public static void Debug(string message)
+    {
+        Logger.Debug(message);
+    }
+
+}
